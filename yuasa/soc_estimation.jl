a7859f53b0fbe746aa34e52e9fa223f7aed97898
Martin Cornejo <martin.cornejo@tum.de>
2025-12-07 19:02:58 +0100

    correct EKF for SOC estimation

--- yuasa/soc_estimation.jl
+++ yuasa/soc_estimation.jl
@@ -1,52 +1,47 @@
 
-# TODO: 
-# - add RCs
-# - add vσ² IN R2 calculation
-
 function dynamics_state(x, u, p, t)
-    # (; Ts, R1, τ1, xid) = p
-    (; kf, Ts, q) = p
-    kfx = ComponentVector(kf.x, kf.p.xid)
-    kfx.rc.v = dynamics_rc(kfx.rc, u.i, (; Ts))
+    (; Ts, R1, τ1, q) = p
     soc = x[1]
-    i = u.î # control
+    vrc = x[2]
 
-    # dx[1] = soc + i * Ts / (q * 3600)
-    # dx[2] = v1 * exp(-Ts / τ1) + i * R1 * (1 - exp(-Ts / τ1))
-    soc⁺ = soc + i * Ts / (q * 3600)
-    SA[soc⁺]
-
+    vrc⁺ = vrc * exp(-Ts / τ1) + u.i * R1 * (1 - exp(-Ts / τ1))
+    soc⁺ = soc + u.î * Ts / (q * 3600)
+    SA[soc⁺, vrc⁺]
 end
 
 function measurement_state(x, u, p, t)
-    (; kf, Δsoc, q) = p
-    (; xid, ocv, r0, rc) = kf.p
+    (; kf, Δsoc, q, zt) = p
+    kfx = ComponentVector(kf.x, kf.p.xid)
     soc = x[1]
-    soc´ = (soc + Δsoc) * q
-    kfx = ComponentVector(kf.x, xid)
-    ocv = measurement_gp(ocv, kfx.ocv, soc´)
-    r0 = measurement_gp(r0, kfx.r0, soc´)
-    v = ocv + u.i * r0  + kfx.rc.v|> SVector{1}
-    v
-    # StatsBase.reconstruct(zt.v, v) |> SVector{1}
+    vrc = x[2]
+    q´ = ((soc - Δsoc) * q - zt.q.mean[1]) / zt.q.scale[1] # transform zt.q !!!
+
+    ocv = measurement_gp(kf.p.ocv, kfx.ocv, q´)
+    r0 = measurement_gp(kf.p.r0, kfx.r0, q´)
+    ocv + u.i * r0 + vrc |> SVector{1}
 end
 
 function R2_state(x, u, p, t)
-    (; kf) = p
-    (; xid, ocv, r0, rc) = kf.p
+    (; kf, Δsoc, q, zt) = p
     soc = x[1]
-    # kfx = ComponentVector(kf.x, xid)
-    ocv = uncertainty_gp(ocv, soc)
-    r0 = uncertainty_gp(r0, soc)
-    ocv + u.i^2 * r0 |> SMatrix{1,1}
+    q´ = ((soc - Δsoc) * q - zt.q.mean[1]) / zt.q.scale[1]
+    ocv = uncertainty_gp(kf.p.ocv, q´)
+    r0 = uncertainty_gp(kf.p.r0, q´)
+    ocv + u.i^2 * r0 + kf.p.vσ² |> SMatrix{1,1}
 end
 
-function build_kf_state(kf1, soc0, Δsoc, q, Ts)
-    x0 = SA[soc0]
-    θ = (; kf=kf1, Ts, q, Δsoc)
+function build_kf_state(kf1, soc0, Δsoc, q, Ts, zt)
+    xid = kf1.p.xid
+    xc = ComponentVector(kf1.x, xid)
 
+    R1 = xc.rc.r
+    τ1 = xc.rc.τ
+
+    θ = (; kf=kf1, Ts, R1, τ1, q, Δsoc, vσ²=1e-2, zt)
+
+    x0 = SA[soc0, 0.0]
     d0 = MvNormal(x0, 1e-5I)
-    R1 = @SMatrix [1e-12;;]
-    ExtendedKalmanFilter(dynamics_state, measurement_state, R1, R2_state, d0; nx=1, nu=1, ny=1, p=θ)
+    R1 = @SMatrix [1e-4 0; 0 1e-5]
+    ExtendedKalmanFilter(dynamics_state, measurement_state, R1, R2_state, d0; nx=2, nu=1, ny=1, p=θ)
 end
 
