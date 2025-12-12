c2f19c8ca235c8b710eace05ff1c0cf78e2bd407
JuanVictorSandalinas <go69lif@mytum.de>
2025-12-12 11:23:28 +0100

    Redactor rgp: Corrected uncertanity, Added cov! for sum of kernels

--- src/rgp.jl
+++ src/rgp.jl
@@ -8,14 +8,21 @@ Statistics.mean(gp::GP, x::Real) = mean_value(gp.mean, x)
 Statistics.cov(gp::GP, x::AbstractVector, y::Real) = gp.kernel.(x, y)
 Statistics.cov(gp::GP, x::Real, y::AbstractVector) = gp.kernel.(x, y)'
 
-function cov!(c::AbstractVector, gp::GP, x::AbstractVector, y::Real)
+function cov!(c::AbstractVector, gp::GP{<:Any,<:KernelSum}, x::AbstractVector, y)
+    fill!(c, 0.0) ### zero(c), with eltype()
+    for kernel in gp.kernel.kernels
+        @. c += kernel(x, y)
+    end
+end
+
+function cov!(c::AbstractVector, gp::GP{<:Any,<:Kernel}, x::AbstractVector, y)
     @. c = gp.kernel(x, y)
 end
 
-struct RGP{bT,BT,RT,cT}
+struct RGP{bT,dT,BT,RT,cT}
     gp::GP
     b0::bT
-    μ0::bT
+    μ0::dT
     Σ0::BT
     Σ0⁻¹::BT
     R1::RT
@@ -24,10 +31,10 @@ end
 
 
 function RGP(gp::GP, b0::T) where T<:AbstractArray
-    nb = length(b0) # 1-dim basis vector (for now)
-
-    μ0 = mean(gp, b0) |> T
-    # μ0 = SVector{nb}(mean(gp, b0))
+    nb = length(b0) # 1 dim basis vector, for now
+    R1 = zeros(nb, nb)
+    
+    μ0 = mean(gp, b0)
     Σ0 = cov(gp, b0) + 1e-6I
     Σ0⁻¹ = inv(Σ0)
 
@@ -37,7 +44,7 @@ function RGP(gp::GP, b0::T) where T<:AbstractArray
         k=DiffCache(similar(b0)),
         k⁻=DiffCache(similar(b0)),
         H=DiffCache(similar(b0')),
-        # Δg=similar(b0), # <- use DiffCache
+        Δg=DiffCache(similar(b0)),
     )
 
     RGP(gp, b0, μ0, Σ0, Σ0⁻¹, R1, cache)
@@ -58,29 +65,24 @@ end
 # dynamics(x, u, p, t) = x
 function measurement_gp(rgp::RGP, g::AbstractArray, b::Real)
     (; gp, b0, μ0, Σ0⁻¹, cache) = rgp
-    # (; k, H) = cache
-    # Δg = get_tmp(cache.Δg, g)
-    k = get_tmp(cache.k, b)
-    H = get_tmp(cache.H, b)
+    k = get_tmp(cache.k, g)
+    H = get_tmp(cache.H, g)
+    Δg = get_tmp(cache.Δg, g)
 
-    # (cov(gp, b, b0) * Σ0⁻¹) * (g - μ0) + mean(gp, b)
-    #        k                    Δg
-    #                H
-    cov!(k, gp, b0, b) # k = cov(gp, b, b0)
+    cov!(k, gp, b0, b) # k .= cov(gp,b0, b)
     mul!(H, k', Σ0⁻¹) # H = k' * Σ0⁻¹
-    Δg = g - μ0
-    # Δg .= g .- μ0
+    Δg .= g - μ0
     muladd(H, Δg, mean(gp, b)) # H * (g - μ0) + m
 end
 
-function uncertainty_gp(rgp::RGP, b::Real)
+function uncertainty_gp(rgp::RGP, g::AbstractArray, b::Real)
     (; gp, b0, Σ0⁻¹, cache) = rgp
-    k = get_tmp(cache.k, b)
-    H = get_tmp(cache.H, b)
-    k⁻ = get_tmp(cache.k⁻, b)
-    cov!(k, gp, b0, b)
-    mul!(H, k', Σ0⁻¹) # H
+    k = get_tmp(cache.k, g)
+    H = get_tmp(cache.H, g)
+    k⁻ = get_tmp(cache.k⁻, g)
+    cov!(k, gp, b0, b) # k .= cov(gp,b0, b)
+    mul!(H, k', Σ0⁻¹)  # H = k' * Σ0⁻¹
     @. k⁻ = -k
-    muladd(H, k⁻, gp.kernel(b, b))
+    muladd(H, k⁻, gp.kernel(b, b)) # H*k⁻ + gp.kernel(b, b)
 end
 
